6fdfff461829e04d50f29daaefccb55301eba50d
tuyangJs <ihanlong@qq.com>
2025-02-02 17:42:04 +0800

    修复前端错误

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { trace, info, error, attachConsole } from '@tauri-apps/plugin-log';
+import {  attachConsole } from '@tauri-apps/plugin-log';
 
 // 启用 TargetKind::Webview 后，这个函数将把日志打印到浏览器控制台
 const detach = await attachConsole();
